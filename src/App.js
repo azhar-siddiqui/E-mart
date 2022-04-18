2c0af26ca07de8448eab4fd44fc610fd7b7e32af
Azhar Sayyed <azhartsiddiqui@gmail.com>
2022-04-18 11:39:40 +0530

    E-mart shopping applicatiuon

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import { Route, Routes} from 'react-router-dom';
+import Products from './components/Products';
+import Product from './components/Product';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Routes>
+      <Route exact path="/" element={<Home/>} />
+      <Route exact path="/products" element={<Products/>} />
+      <Route exact path="/products/:id" element={<Product/>} />
+    </Routes>
+      
+    </>
+  );
+}
+
+export default App;
